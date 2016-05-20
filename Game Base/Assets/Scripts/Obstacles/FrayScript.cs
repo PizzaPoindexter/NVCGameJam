bb8c361dc2a485d46218849b3b8a6cd9413164f0
Travis Tatsch <travis.tatsch@gmail.com>
2016-05-20 12:15:29 -0500

    Add Obstacle folder w/respective scripts and Obstacle code to Gameconstroller.cs

---
+++ Game Base/Assets/Scripts/Obstacles/FrayScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class FrayScript : MonoBehavious {
+	void Start () {
+	//Use this for instantiation
+	}
+
+	void Update () {
+	//Update called once per frame
+	}
+}
---
+++ Game Base/Assets/Scripts/Obstacles/WebScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class /* What does Spider-Man use to remember his lines? a */ WebScript : MonoBehaviour {
+
+	void Start () {
+	//Used for initialization
+	}
+
+	void Update () {
+	//Update called once per frame
+	}	
+}
