5191b5c48588937f4196f206aa5010fbef7c83d9
oStaiko <oStaiko45@gmail.com>
2016-05-20 14:19:44 -0500

    Models and idk really
    random stuff but whatever

--- Game Base/Assets/Scripts/Obstacles/FrayScript.cs
+++ Game Base/Assets/Scripts/Obstacles/FrayScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class FrayScript : MonoBehavious {
+public class FrayScript : MonoBehaviour {
 	void Start () {
 	//Use this for instantiation
 	}
