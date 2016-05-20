c8925e647741901ecf4cbfa2c7f25c8109478dd3
shadowraive <coolcliff892@Yahoo.com>
2016-05-20 14:47:49 -0500

    fixed typos, added bird dropping prefab,dropgen

---
+++ Game Base/Assets/Scripts/Dropgenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dropgenerator : MonoBehaviour {
+
+
+	public int dropRate = 75;
+	public int frames;
+	public bool dropped = false;
+	public GameObject poop;
+	// Update is called once per frame
+	void Update () {
+		if (!dropped) {
+			frames++;
+			if (frames >= dropRate) {
+				Instantiate (poop, new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+				dropped = true;
+			}
+		}
+	}
+}
+
+				
--- Game Base/Assets/Scripts/PowerUpsDowns/HatScript.cs
+++ Game Base/Assets/Scripts/PowerUpsDowns/HatScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class HatScript : MonoBehavious {
+public class HatScript : MonoBehaviour {
 	void Start () {
 	//used for initialization
 	}
