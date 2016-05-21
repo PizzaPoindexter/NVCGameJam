b6a4c88253780e49e4fb2d336fc5cf7a2415a180
shadowraive <coolcliff892@Yahoo.com>
2016-05-21 13:38:42 -0500

    animations

---
+++ Game Base/Assets/Scripts/DestroyTriggered.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTriggered : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		Destroy(this.gameObject);
+	}
+}
